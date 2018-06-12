f37fb01771b3a0a21fa8841bbd74510410e3342e
Ant-Pavel <artem.pavel2016@yandex.ru>
2018-06-12 15:58:40 +0200

    Added playground project

---
+++ playground_html/js/user.js
@@ -0,0 +1,8 @@
+"use strict";
+$(document).ready(function() {
+    // // ********************
+    // MatchHeight
+    // // ********************
+    $('.prod-type__rel-list').matchHeight();
+
+});
