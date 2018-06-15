e0ac2afdba0f0e2aafa429deaaafbe51ec53befc
Ant-Pavel <artem.pavel2016@yandex.ru>
2018-06-15 21:10:52 +0200

    Added bau project

---
+++ bau_html/js/user.js
@@ -0,0 +1,12 @@
+"use strict";
+$(document).ready(function() {
+    // // ********************
+    // Слайдер
+    // // ********************
+    $('#cascade-slider').cascadeSlider({});
+
+    // // ********************
+    // MatchHeight
+    // // ********************
+    // $('.why__name').matchHeight();
+ });
