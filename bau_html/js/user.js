2ccb581a810af2d3790c1ea04ce5c6174c6d4695
Ant-Pavel <artem.pavel2016@yandex.ru>
2018-06-18 09:02:06 +0200

    Added popup

--- bau_html/js/user.js
+++ bau_html/js/user.js
@@ -5,5 +5,9 @@ $(document).ready(function() {
     // // ********************
     $('#cascade-slider').cascadeSlider({});
 
+	$('.field-select__input').on('change', function() {
+		var val = $(this).children("option").filter(":selected").text();
+	$('.field-select__fake-option .fake-text').html(val);
+	});
 
  });
